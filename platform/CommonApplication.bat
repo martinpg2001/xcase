cdefd3e0c718ca8a044f6eb7449abf7eab6e6f47
Martin Gilchrist <martin.gilchrist@intapp.com>
2019-05-17 15:04:49 -0700

    Update CommonApplication.bat

--- platform/CommonApplication.bat
+++ platform/CommonApplication.bat
@@ -1,5 +1,5 @@
 CALL SetClassPath.bat
-REM java -Djsse.enableSNIExtension=false -classpath %CLASSPATH% com.xcase.common.CommonApplication -h -D C:/temp/Headers.txt -d "dummy data" -F q=sausage -H "Accept:application/json" -H "Content-Type:application/json" -o C:/temp/Output.txt -u martin:password -X GET https://www.google.com
+java -Djsse.enableSNIExtension=false -classpath %CLASSPATH% com.xcase.common.CommonApplication -h -D C:/temp/Headers.txt -d "dummy data" -F q=sausage -H "Accept: application/json" -H "Content-Type: application/json" -o C:/temp/Output.txt -u martin:password -X GET https://www.google.com
 
 java -Djsse.enableSNIExtension=false -classpath %CLASSPATH% com.xcase.common.CommonApplication -D C:/temp/Headers.txt -d "dummy data" -F q=sausage -H "Accept: application/json" -H "Content-Type: application/json" -o C:/temp/Output.txt -u martin:password -X GET https://www.google.com
 
