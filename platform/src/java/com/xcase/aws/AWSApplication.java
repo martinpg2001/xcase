cce46e4ee98de0836218107805f6e98a597fe8bf
Martin Gilchrist <martinpg@javamarket.com>
2019-06-07 18:13:14 -0700

    Added basic S3 operations.

--- platform/src/java/com/xcase/aws/AWSApplication.java
+++ platform/src/java/com/xcase/aws/AWSApplication.java
@@ -1,21 +1,26 @@
 package com.xcase.aws;
 
+import com.xcase.aws.impl.simple.core.AWSConfigurationManager;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.auth.credentials.AwsCredentials;
 import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.http.SdkHttpClient;
+import software.amazon.awssdk.http.apache.ApacheHttpClient;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.*;
 import software.amazon.awssdk.services.s3.model.Bucket;
+import software.amazon.awssdk.services.s3.model.CreateBucketRequest;
+import software.amazon.awssdk.services.s3.model.CreateBucketResponse;
+import software.amazon.awssdk.services.s3.model.DeleteBucketRequest;
+import software.amazon.awssdk.services.s3.model.DeleteBucketResponse;
 import software.amazon.awssdk.services.s3.model.ListBucketsResponse;
+import software.amazon.awssdk.services.s3.model.S3Request;
 
 import java.lang.invoke.MethodHandles;
 import java.util.List;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
-import com.xcase.aws.impl.simple.core.AWSConfigurationManager;
 
 public class AWSApplication {
     /**
@@ -30,8 +35,9 @@ public class AWSApplication {
             LOGGER.debug("accessKey is " + accessKey);
             String secretKey = AWSConfigurationManager.getConfigurationManager().getLocalConfig().getProperty("aws.aws_secret_access_key");
             LOGGER.debug("secretKey is " + secretKey);
+            SdkHttpClient apacheSdkHttpClient = ApacheHttpClient.builder().build();
             AwsCredentialsProvider awsCredentialsProvider = StaticCredentialsProvider.create(AwsBasicCredentials.create(accessKey, secretKey));
-            S3ClientBuilder s3ClientBuilder = S3Client.builder().region(Region.US_WEST_2).credentialsProvider(awsCredentialsProvider);
+            S3ClientBuilder s3ClientBuilder = S3Client.builder().httpClient(apacheSdkHttpClient).region(Region.US_WEST_2).credentialsProvider(awsCredentialsProvider);
             S3Client s3Client = s3ClientBuilder.build();
             LOGGER.debug("created s3Client");
             ListBucketsResponse listBucketsResponse = s3Client.listBuckets();
@@ -42,6 +48,29 @@ public class AWSApplication {
             } else {
                 LOGGER.debug("bucketList is null");
             }
+            
+            String bucketName = "com.xcase.testbucket";
+            CreateBucketRequest createBucketRequest = CreateBucketRequest.builder().bucket(bucketName).build();
+            CreateBucketResponse createBucketResponse = s3Client.createBucket(createBucketRequest);
+            listBucketsResponse = s3Client.listBuckets();
+            LOGGER.debug("got listBucketsResponse " + listBucketsResponse.toString());
+            bucketList = listBucketsResponse.buckets();
+            if (bucketList != null) {
+                LOGGER.debug("bucketList size is " + bucketList.size());
+            } else {
+                LOGGER.debug("bucketList is null");
+            }
+            
+            DeleteBucketRequest deleteBucketRequest = DeleteBucketRequest.builder().bucket(bucketName).build();
+            DeleteBucketResponse deleteBucketResponse = s3Client.deleteBucket(deleteBucketRequest);
+            listBucketsResponse = s3Client.listBuckets();
+            LOGGER.debug("got listBucketsResponse " + listBucketsResponse.toString());
+            bucketList = listBucketsResponse.buckets();
+            if (bucketList != null) {
+                LOGGER.debug("bucketList size is " + bucketList.size());
+            } else {
+                LOGGER.debug("bucketList is null");
+            }            
         } catch (Exception e) {
             LOGGER.warn("exception getting bucket list: " + e.getMessage());
         }
