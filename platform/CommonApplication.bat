e035c79e0c4469e9f70ea8832182c4432d1d0a4f
Martin Gilchrist <martinpg@outlook.com>
2020-06-30 16:52:58 -0700

    Fixed Guava security issue, and updated SQS support.

--- platform/CommonApplication.bat
+++ platform/CommonApplication.bat
@@ -3,4 +3,4 @@ java -Djsse.enableSNIExtension=false -classpath %CLASSPATH% com.xcase.common.Com
 
 java -Djsse.enableSNIExtension=false -classpath %CLASSPATH% com.xcase.common.CommonApplication -D C:/temp/Headers.txt -d "dummy data" -F q=sausage -H "Accept: application/json" -H "Content-Type: application/json" -o C:/temp/Output.txt -u martin:password -X GET https://www.google.com
 
-java -Djsse.enableSNIExtension=false -classpath %CLASSPATH% com.xcase.common.CommonApplication -F file=@C:/temp/TestApplication.txt -X POST http://localhost:8080/examples/jsp/upload/Action_file_upload.jsp
+java -Djsse.enableSNIExtension=false -classpath %CLASSPATH% com.xcase.common.CommonApplication -F file=@./docs/TestFile.txt -X POST http://localhost:8080/examples/jsp/upload/Action_file_upload.jsp
